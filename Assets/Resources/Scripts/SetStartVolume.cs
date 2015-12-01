b32855e0ee52e7655775c9b43c59d173a40ef77c
Christopher <ChrisJKZack@Gmail.com>
2015-12-01 14:27:51 -0500

    Updating prefabs and animations

---
+++ Assets/Resources/Scripts/SetStartVolume.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStartVolume : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start ()
+    {
+        if (GameObject.Find("MusiceManager"))
+        {
+            GameObject.Find("MusicManager").GetComponent<MusicManager>().ChangeVolume(PlayerPrefManager.GetMasterVolume());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
